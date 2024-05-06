c1d4baedef3343f6bc9e43c4fcc3205cb413e352
Phani <officialphani.cse@gmail.com>
2024-05-06 19:20:22 +0530

    Update URL

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <BrowserRouter> 
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
+
+reportWebVitals();
