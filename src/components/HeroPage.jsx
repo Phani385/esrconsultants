2773641037a1514839c9104a7df76c998b36fa1a
Phani <officialphani.cse@gmail.com>
2024-05-06 16:28:09 +0530

    Fix Hero Section Issues

--- src/components/HeroPage.jsx
+++ src/components/HeroPage.jsx
@@ -11,10 +11,7 @@ const HeroPage = ({ imageUrl }) => {
       <div className="text-center text-white w-[90%] lg:w-[60%]">
         <h1 className="mb-5 text-3xl font-bold text-white lg:text-4xl xl:text-5xl">
           ESR Corpotate Consultants LLP
-          <br />{" "}
-          <span className="text-secondaryColor-500">
-            Quality is our business plan
-          </span>
+          <br /> Quality is our business plan
         </h1>
         <p className="mb-3 lg:mb-5">
           A Reputed Tax Consultant & Chartered Accountant, who has helped many
