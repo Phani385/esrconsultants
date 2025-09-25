807fd8740f1fe385bd30e460e8ab2ab3e93b19cf
esr-corp <esrcorporatesllp@gmail.com>
2025-09-25 14:03:40 +0530

    Update website content

--- src/components/HomeTeamCard.jsx
+++ src/components/HomeTeamCard.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 export const HomeTeamCard = ({ imageUrl, name, description }) => {
   return (
-    <li className="w-full">
-      <img src={imageUrl} className="h-[300px] w-full rounded-md" />
+    <li className="w-[50%] mx-auto ">
+      <img src={imageUrl} className="h-[300px] w-full rounded-md " />
       <div className="mt-3">
         <h3 className="text-2xl font-bold text-secondaryColor-500">{name}</h3>
         <p className="text-gray-500">{description}</p>
